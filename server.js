0c58f134b449453e2d84aa584d41a5a65a0f9148
Hack Reactor Students <accounts@hackreactor.com>
2016-02-10 14:29:19 -0800

    Server test

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var app = require('./server-config.js');
 
-var port = process.env.PORT;
+var port = 4568;
 
 app.listen(port);
 
-console.log('Server now listening on port ' + port);
+console.log('Joey is now listening on port ' + port);
